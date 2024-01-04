d68ddf4f7662c04abd6309e3723cabd89e24ad11
atharhasan <athar_h12@yahoo.com>
2024-01-04 10:42:46 +0200

    - add di - add api constants - add network layer - integrate api for login - create login request body model & login response model

--- lib/core/routing/app_router.dart
+++ lib/core/routing/app_router.dart
@@ -1,7 +1,10 @@
 
+import 'package:doc_app/core/di/dependency_injection.dart';
 import 'package:doc_app/core/routing/routes.dart';
+import 'package:doc_app/features/login/logic/cubit/login_cubit.dart';
 import 'package:doc_app/features/login/ui/screens/login_screen.dart';
 import 'package:doc_app/features/on_boarding/on_boarding_screen.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter/material.dart';
 
 class AppRouter {
@@ -10,7 +13,10 @@ class AppRouter {
       case Routes.onBoardingScreen:
         return MaterialPageRoute(builder: (_) => const OnBoardingScreen());
       case Routes.loginScreen:
-        return MaterialPageRoute(builder: (_) => const LoginScreen());
+        return MaterialPageRoute(builder: (_) => BlocProvider(
+          create: (context) => getIt<LoginCubit>(),
+          child: const LoginScreen(),
+        ));
       default:
         return MaterialPageRoute(
             builder: (_) => Scaffold(
--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,11 @@
+import 'package:doc_app/core/di/dependency_injection.dart';
 import 'package:doc_app/core/routing/app_router.dart';
 import 'package:doc_app/doc_app.dart';
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(DocApp(appRouter: AppRouter(),));
+  setupGetIt();
+  runApp(DocApp(
+    appRouter: AppRouter(),
+  ));
 }
-
-
